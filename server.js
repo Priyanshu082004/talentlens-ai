7da83e5a40715f4b75c999882cc713ad2a298c3a
Priyanshu082004 <priyanshuu202004@gmail.com>
2026-06-13 23:34:19 +0530

    feat: add server and app initialization files

---
+++ server.js
@@ -0,0 +1,12 @@
+import { app } from './src/app';
+
+require('dotenv').config();
+
+const app = require('./src/app').app;
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
